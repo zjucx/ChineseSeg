b2449e06c0fdb58c509b418ea9b9d6e77fe9a664
zjucx <862575451@qq.com>
2017-09-23 23:37:50 +0800

    it works well now

--- main.go
+++ main.go
@@ -1,12 +1,12 @@
 package main
 
 import (
-  "fmt"
+  "suffixtree"
 )
 
 func main() {
-  str := "我们是中国人"
-  r := []rune(str)
-  fmt.Println("rune=", string(r[0]))
   //fmt.Printf(str[0])
+  suffix := suffixtree.New()
+  suffix.Build("abcabxabcd")
+  suffix.Print("abcabxabcd")
 }
--- src/suffixtree/suffixtree.go
+++ src/suffixtree/suffixtree.go
@@ -4,12 +4,14 @@
 package suffixtree
 
 import(
-
+  "fmt"
 )
 
 type TreeNode struct{
   Sidx  int
   Eidx  int
+  words string
+  SuffixLink *TreeNode
   Child map[string]*TreeNode
 }
 
@@ -28,6 +30,19 @@ type SuffixTree struct {
   Remainder int
 }
 
+func New() *SuffixTree{
+  return &SuffixTree{
+    Root: &TreeNode{Sidx: 0, Eidx: 0, Child: make(map[string]*TreeNode)},
+    CurStep: 0,
+    ActiveNode: nil,
+    ActiveEdge: "",
+    ActiveLen: 0,
+    //剩余后缀数（remainder），是一个整数，代表着还需要插入多少个新的后缀
+    Remainder: 1,
+  }
+}
+
+
 func (st *SuffixTree) Build(str string) {
   // prepare for chisese sentence
   sentence := []rune(str)
@@ -39,43 +54,52 @@ func (st *SuffixTree) Build(str string) {
     word := string(value)
 
     // if cur word is frist word of suffix in one edge in cur root
-    if v, ok := root.Child[word]; ok && st.Remainder == 1 {
+    if _, ok := root.Child[word]; ok && st.Remainder == 1 {
+        st.ActiveNode = root
         st.ActiveEdge = word
         st.ActiveLen = 1
         st.Remainder += 1
-        return
+        fmt.Printf("#:%d ActiveEdge:%s ActiveLen:%d Remainder:%d\n", st.CurStep, st.ActiveEdge, st.ActiveLen, st.Remainder)
+        continue
     }
 
-    if v, ok := root.Child[word]; !ok && st.Remainder == 1 {
+    // if cur word is not in the root child and is also not a suffix, then add a child
+    if _, ok := root.Child[word]; !ok && st.Remainder == 1 {
       child := st.NewChild(index)
       root.Child[word] = child
-      return
+      fmt.Println("add new child " + word)
+      continue
     }
 
+    // if cur word is included by a child then add modify the variables
     if st.Remainder > 1 && value == sentence[st.ActiveNode.Sidx+st.ActiveLen] {
       st.Remainder += 1
       st.ActiveLen += 1
-      nodeLen := st.ActiveNode.Eidx - st.ActiveNode.Bidx
+      nodeLen := st.ActiveNode.Child[st.ActiveEdge].Eidx - st.ActiveNode.Child[st.ActiveEdge].Sidx
       if nodeLen > 0 &&  st.ActiveLen > nodeLen {
         st.ActiveNode = st.ActiveNode.Child[st.ActiveEdge]
         st.ActiveEdge = word
         st.ActiveLen = 1
       }
-      return
+      fmt.Printf("#:%d ActiveEdge:%s ActiveLen:%d Remainder:%d\n", st.CurStep, st.ActiveEdge, st.ActiveLen, st.Remainder)
+      continue
     }
 
-    lastSplit = nil
+    // reminder is larger than 1 and cur word is not match the suffix tnen split the suffix tree
+    var lastSplit *TreeNode
     for st.Remainder >= 1 {
       // insert root node child
       if st.Remainder == 1 {
         child := st.NewChild(index)
         root.Child[word] = child
-        st.ActiveEdge = nil
-        return
+        st.ActiveEdge = ""
+        fmt.Println("add new child " + word)
+        fmt.Printf("#:%d ActiveEdge:%s ActiveLen:%d Remainder:%d\n", st.CurStep, st.ActiveEdge, st.ActiveLen, st.Remainder)
+        break
       }
 
       // current split node
-      curSplit := st.ActiveNode[st.ActiveEdge]
+      curSplit := st.ActiveNode.Child[st.ActiveEdge]
       st.Split(curSplit, str)
 
       // update active point
@@ -89,7 +113,7 @@ func (st *SuffixTree) Build(str string) {
             active_edge 被设置为即将被插入的新后缀的首字符；
             active_length 减 1；
         */
-        st.ActiveEdge = lastword
+        st.ActiveEdge = string(lastword)
         st.ActiveLen -= 1
       } else if suffixLink != nil{
         /*  Rule 3
@@ -110,20 +134,48 @@ func (st *SuffixTree) Build(str string) {
           后缀连接通过一条虚线来表示。
       */
       if lastSplit != nil {
-        curSplit.SuffixLink = lastSplit
+        lastSplit.SuffixLink = curSplit
       }
       lastSplit = curSplit
+      fmt.Printf("#:%d ActiveEdge:%s ActiveLen:%d Remainder:%d\n", st.CurStep, st.ActiveEdge, st.ActiveLen, st.Remainder)
     }
   }
 }
 
 func (st *SuffixTree) Split(node *TreeNode, str string) {
-  node.Flag = 1
-  node.Eidx = node.Bidx + st.ActiveLen
+  node.Eidx = node.Sidx + st.ActiveLen
   child1 := st.NewChild(node.Eidx)
   child2 := st.NewChild(st.CurStep)
 
   sentence := []rune(str)
   node.Child[string(sentence[node.Eidx])] = child1
   node.Child[string(sentence[st.CurStep])] = child2
+  fmt.Println("add new child " + string(sentence[node.Eidx]))
+  fmt.Println("add new child " + string(sentence[st.CurStep]))
+}
+
+func (st *SuffixTree) NewChild(index int) *TreeNode{
+  return &TreeNode{
+    Sidx: index,
+    Eidx: 0,
+    words: "",
+    SuffixLink: nil,
+    Child: make(map[string]*TreeNode),
+  }
+}
+
+func (st *SuffixTree) Print(str string) {
+  sentence := []rune(str)
+  print(st.Root, sentence, 0)
+}
+
+func print(root *TreeNode, sentence []rune, ph int) {
+  for _, node := range root.Child {
+    if node.Eidx == 0 {
+      fmt.Printf("%*s->%s\n", ph*4, "", string(sentence[node.Sidx:]))
+    } else {
+      fmt.Printf("%*s->%s\n", ph*4, "", string(sentence[node.Sidx:node.Eidx]))
+    }
+    print(node, sentence, ph+1)
+  }
 }
